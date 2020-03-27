603d1a0cc2f2bf0f22afa57bf69149b9db70c454
Hiroaki-Nakajima <h.19860820@gmail.com>
2020-03-27 21:02:01 +0900

    条件分岐追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     all_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
     @my_ranks = all_ranks.select{ |post| post.user_id == current_user.id }
     @user_ranks = all_ranks.select{ |post| post.user_id == @user.id }
+    @top_rank = @user_ranks.first
   end
 
   def edit
